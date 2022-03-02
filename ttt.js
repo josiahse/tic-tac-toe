2c87ba0f13209b639925e7c3b6afcfc5f2e22324
josiahse <josiah@josiahse.com>
2022-03-02 13:40:55 -0700

    added new computerMove and checkEmpty functions

--- ttt.js
+++ ttt.js
@@ -27,42 +27,57 @@ let checkWinCond = (tile) => {
 	}
 };
 
+const checkEmpty = (index) => {
+	return board[index] !== 'x' && board[index] !== 'o';
+};
+
+const computerMove = () => {
+	let compIndex = Math.floor(Math.random() * 9);
+	while (!checkEmpty(compIndex)) {
+		console.log('checking tile');
+		compIndex = Math.floor(Math.random() * 9);
+	}
+
+	compMoveTile = `t${compIndex + 1}`;
+	// console.log(compMoveTile);
+
+	$(`.${compMoveTile}`).toggleClass('empty');
+	$(`.${compMoveTile}`).find('>:last-child').toggleClass('hide');
+	board[compMoveTile[1] - 1] = 'o';
+	console.log(`c: ${board}`);
+	if (checkWinCond(compMoveTile[1])) {
+		$('.computer').toggleClass('hide');
+	}
+};
+
 $('.empty').on('click', function (event) {
-	event.preventDefault;
-	board.forEach((tile) => {
-		if ($(this).hasClass(`${tile}`)) {
-			$(this).toggleClass('empty');
-			tileNum = tile[1];
-			board[tileNum - 1] = 'x';
-			$(this).find('>:first-child').toggleClass('hide');
-			if (checkWinCond(tileNum)) {
-				$('.player').toggleClass('hide');
-			} else {
-				compMoveTile = `t${Math.ceil(Math.random() * 9)}`;
-				console.log(compMoveTile);
-				while (
-					compMoveTile === tile ||
-					!$(`.${compMoveTile}`).hasClass('empty')
-				) {
-					compMoveTile = `t${Math.ceil(Math.random() * 9)}`;
-				}
-				$(`.${compMoveTile}`).toggleClass('empty');
-				$(`.${compMoveTile}`).find('>:last-child').toggleClass('hide');
-				board[compMoveTile[1] - 1] = 'o';
-				if (checkWinCond(compMoveTile[1])) {
-					$('.computer').toggleClass('hide');
-				}
+	if ($(this).hasClass('empty')) {
+		let tileNum = 0;
+		event.preventDefault;
+		$(this).toggleClass('empty');
+		board.forEach((tile) => {
+			if ($(this).hasClass(`${tile}`)) {
+				tileNum = tile[1];
+				board[tileNum - 1] = 'x';
+				console.log(`p: ${board}`);
 			}
+		});
+		$(this).find('>:first-child').toggleClass('hide');
+
+		if (checkWinCond(tileNum)) {
+			$('.player').toggleClass('hide');
+		} else {
+			computerMove();
 		}
-	});
+	}
 });
 
 $('.reset-button').on('click', function (event) {
 	event.preventDefault;
-    $('div').addClass('empty');
-    $('i').addClass('hide');
-    $('article>p').addClass('hide');
-    for (let i = 0; i < board.length; i++){
-        board[i] = `t${i+1}`;
-    }
+	$('div').addClass('empty');
+	$('i').addClass('hide');
+	$('article>p').addClass('hide');
+	for (let i = 0; i < board.length; i++) {
+		board[i] = `t${i + 1}`;
+	}
 });
